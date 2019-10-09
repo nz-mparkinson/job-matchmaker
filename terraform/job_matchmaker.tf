ee0251adf71ca28f3f174f72c936b4b3a2212a6a
nz-mparkinson <nz.mparkinson@gmail.com>
2019-10-09 23:01:31 +0200

    Added Terraform config for AWS ELB, S3 Bucket and started on DB/User create

--- terraform/job_matchmaker.tf
+++ terraform/job_matchmaker.tf
@@ -3,30 +3,46 @@ provider "aws" {
   region     = "us-east-1"
 }
 
+#Define a EC2 Instance
 resource "aws_instance" "apache1" {
   ami           = "ami-02eac2c0129f6376b"
   instance_type = "t2.micro"
+  key_name      = "aws-key-pair"
   tags = {
     Name = "apache1"
   }
+  root_block_device {
+    delete_on_termination = "true"
+  }
 }
 
+#Define a EC2 Instance
 resource "aws_instance" "apache2" {
   ami           = "ami-02eac2c0129f6376b"
   instance_type = "t2.micro"
+  key_name      = "aws-key-pair"
   tags = {
     Name = "apache2"
   }
-}
-
-resource "aws_instance" "apache3" {
-  ami           = "ami-02eac2c0129f6376b"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "apache3"
+  root_block_device {
+    delete_on_termination = "true"
   }
 }
 
+#Define a EC2 Instance
+resource "aws_instance" "apache3" {
+  ami           = "ami-02eac2c0129f6376b"
+  instance_type = "t2.micro"
+  key_name      = "aws-key-pair"
+  tags = {
+    Name = "apache3"
+  }
+  root_block_device {
+    delete_on_termination = "true"
+  }
+}
+
+#Define a RDS Instance
 resource "aws_db_instance" "postgresql1" {
   allocated_storage    = 20
   storage_type         = "gp2"
@@ -38,8 +54,132 @@ resource "aws_db_instance" "postgresql1" {
   password             = "postgres"
   parameter_group_name = "default.postgres11"
   identifier           = "postgresql1"
+  publicly_accessible  = "true"
   skip_final_snapshot  = "true"
 }
+
+#Get the details of the AWS service account
+data "aws_elb_service_account" "main" {}
+
+#Define a S3 Bucket
+resource "aws_s3_bucket" "storage1" {
+  bucket = "job-matchmaker-storage1"
+  acl    = "private"
+  force_destroy           = "true"
+
+  tags = {
+    Name        = "job-matchmaker-storage1"
+    Environment = "Dev"
+  }
+
+  policy = <<POLICY
+{
+  "Id": "Policy",
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:PutObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::job-matchmaker-storage1/*",
+      "Principal": {
+        "AWS": [
+          "${data.aws_elb_service_account.main.arn}"
+        ]
+      }
+    }
+  ]
+}
+POLICY
+}
+
+#Define a EC2 Load Balancer
+resource "aws_elb" "loadbalancer1" {
+  name               = "loadbalancer1"
+  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e", "us-east-1f"]
+
+  access_logs {
+    bucket        = "job-matchmaker-storage1"
+    bucket_prefix = "loadbalancer1"
+    interval      = 60
+  }
+
+  listener {
+#    instance_port     = 8000
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+#  listener {
+#    instance_port      = 8000
+#    instance_protocol  = "http"
+#    lb_port            = 443
+#    lb_protocol        = "https"
+#    ssl_certificate_id = "arn:aws:iam::123456789012:server-certificate/certName"
+#  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/search.html"
+    interval            = 30
+  }
+
+  instances                   = ["${aws_instance.apache1.id}", "${aws_instance.apache2.id}", "${aws_instance.apache3.id}"]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "loadbalancer1"
+  }
+}
+
+
+
+
+
+/*
+#Configure the PostgreSQL provider
+provider "postgresql" {
+  host = "${aws_db_instance.postgresql1.address}"
+  username = "postgres"
+  password = "postgres"
+  connect_timeout = 15
+  sslmode = "require"
+  superuser = "true"
+}
+
+#Define a PostgreSQL user
+resource "postgresql_role" "job_matchmaker" {
+  name     = "job_matchmaker"
+  login    = true
+  password = "job_matchmaker"
+#  encrypted_password = true
+}
+
+#Define a PostgreSQL database
+resource "postgresql_database" "job_matchmaker" {
+  name              = "job_matchmaker"
+  owner             = "job_matchmaker"
+  connection_limit  = -1
+  allow_connections = true
+}
+
+#Grant a PostgreSQL user privileges on a database
+resource postgresql_grant "job_matchmaker" {
+  database    = "job_matchmaker"
+  role        = "job_matchmaker"
+  schema      = "public"
+  object_type = "sequence"
+  privileges  = ["ALL"]
+}
+*/
 
 
 
