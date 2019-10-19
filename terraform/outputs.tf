a96be9458db0119214566521bcbb7d1be21d94f1
nz-mparkinson <nz.mparkinson@gmail.com>
2019-10-19 19:29:56 +0200

    Added additional outputs for ELB/EC2 DNS names

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,9 +10,21 @@ output "ami" {
   description = "The AMI used"
 }
 
-#Output the RDS instance
+#Output the RDS DNS name
 output "rds_instance" {
   value = "${aws_db_instance.postgresql1.address}"
   description = "The DNS name of the RDS instance"
+}
+
+#Output the ELB DNS name
+output "elb_instance" {
+  value = "${aws_elb.loadbalancer1.dns_name}"
+  description = "The DNS name of the Elastic Load Balancer"
+}
+
+#Output the EC2 instance DNS names
+output "ec2_instances" {
+  value = "${aws_instance.web_servers.*.public_dns}"
+  description = "The DNS names of the EC2 instances"
 }
 
