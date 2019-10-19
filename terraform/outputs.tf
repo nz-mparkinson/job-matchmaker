4271c83fef6be413d67d1ff85d05502284abb969
nz-mparkinson <nz.mparkinson@gmail.com>
2019-10-19 18:39:56 +0200

    Added descriptions to inputs/outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,18 @@
 #Output the region
 output "region" {
   value = var.region
+  description = "The AWS region used"
 }
 
 #Output the AMI used
 output "ami" {
   value = var.amis[var.region]
+  description = "The AMI used"
 }
 
 #Output the RDS instance
 output "rds_instance" {
   value = "${aws_db_instance.postgresql1.address}"
+  description = "The DNS name of the RDS instance"
 }
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,6 +2,7 @@
 variable "region" {
   type    = "string"
   default = "us-east-1"
+  description = "The AWS region used"
 }
 
 #Define what AMI to use for AWS EC2 instances depending on the region
@@ -11,5 +12,6 @@ variable "amis" {
     "us-east-1" = "ami-02eac2c0129f6376b"          #CentOS7 - us-east-1
     "us-west-2" = "ami-0f2b4fc905b0bd1f1"          #CentOS7 - us-east-2
   }
+  description = "A map of what AMI to use depending on the AWS region"
 }
 
